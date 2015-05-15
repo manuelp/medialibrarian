3a6c764d554553e221c5dbff39d99eddc383a01a
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2015-05-15 20:05:28 +0200

    Simplified a bit.

--- src/main/java/me/manuelp/medialibrarian/MediaLibrarian.java
+++ src/main/java/me/manuelp/medialibrarian/MediaLibrarian.java
@@ -1,6 +1,5 @@
 package me.manuelp.medialibrarian;
 
-import fj.F;
 import fj.data.List;
 
 import java.io.IOException;
@@ -19,24 +18,23 @@ public class MediaLibrarian {
     Configuration conf = new Configuration(source, archive);
     System.out.println("Configuration: " + conf.toString());
 
+    List<Path> files = findInterestingFiles(conf.getDir());
+    System.out.println("Interesting: " + files);
+  }
+
+  private static List<Path> findInterestingFiles(Path rootDir)
+      throws IOException {
     final ArrayList<Path> files = new ArrayList<>();
-    Files.walkFileTree(conf.getDir(), new SimpleFileVisitor<Path>() {
+    Files.walkFileTree(rootDir, new SimpleFileVisitor<Path>() {
       @Override
       public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
           throws IOException {
-        files.add(file);
+        String filename = file.getFileName().toString();
+        if (EXTENSIONS.filter(e -> filename.endsWith("." + e)).isNotEmpty())
+          files.add(file);
         return FileVisitResult.CONTINUE;
       }
     });
-    List<Path> interesting = List.iterableList(files).filter(isInteresting());
-
-    System.out.println("Interesting: " + interesting);
-  }
-
-  private static F<Path, Boolean> isInteresting() {
-    return p -> {
-      String filename = p.getFileName().toString();
-      return EXTENSIONS.filter(e -> filename.endsWith("." + e)).isNotEmpty();
-    };
+    return List.iterableList(files);
   }
 }
