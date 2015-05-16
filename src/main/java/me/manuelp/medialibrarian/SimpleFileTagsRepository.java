fa062ac171fb9e7b319070480b8803fa8efae79e
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2015-05-16 14:09:03 +0200

    Added reading operation to `TagsRepository` and implemented it in `SimpleFileTagsRepository`.

--- src/main/java/me/manuelp/medialibrarian/SimpleFileTagsRepository.java
+++ src/main/java/me/manuelp/medialibrarian/SimpleFileTagsRepository.java
@@ -1,9 +1,14 @@
 package me.manuelp.medialibrarian;
 
+import fj.data.List;
 import me.manuelp.medialibrarian.data.MediaFile;
 import me.manuelp.medialibrarian.data.Tag;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class SimpleFileTagsRepository implements TagsRepository {
   private File tagsFile;
@@ -26,9 +31,24 @@ public class SimpleFileTagsRepository implements TagsRepository {
     }
   }
 
+  @Override
+  public List<MediaFile> read() {
+    ArrayList<MediaFile> files = new ArrayList<>();
+    try {
+      Files.lines(tagsFile.toPath()).map(l -> l.split(":")).map(t -> {
+        Path file = Paths.get(t[0]);
+        List<Tag> tags = List.list(t[1].split(",")).map(Tag::new);
+        return new MediaFile(file, tags);
+      }).forEach(mf -> files.add(mf));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return List.iterableList(files);
+  }
+
   private String format(MediaFile mf) {
     String tags = mf.getTags().map(Tag::getCode).intersperse(",")
         .foldLeft((a, b) -> a + b, "");
-    return String.format("%s:%s", mf.getPath().getFileName().toString(), tags);
+    return String.format("%s\t%s", mf.getPath().getFileName().toString(), tags);
   }
 }
--- src/main/java/me/manuelp/medialibrarian/TagsRepository.java
+++ src/main/java/me/manuelp/medialibrarian/TagsRepository.java
@@ -1,7 +1,10 @@
 package me.manuelp.medialibrarian;
 
+import fj.data.List;
 import me.manuelp.medialibrarian.data.MediaFile;
 
 public interface TagsRepository {
   void write(MediaFile mf);
+
+  List<MediaFile> read();
 }
