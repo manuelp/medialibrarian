46f4721350d581c7d0f6f30ba4111cccf4ca79e7
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2015-05-16 02:20:03 +0200

    Used jline2 for console I/O.

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ A CLI client to classify and archive media files.
 
 ## Changelog
 
-### 0.0.2-SNAPSHOT
+### 0.0.2
 
 * Added filename and progress printing while viewing videos.
+* Used jline2 to print messages, show prompts, read stuff with some more sophisticated editing capabilities.
 
 ### 0.0.1
 
--- src/main/java/me/manuelp/medialibrarian/Main.java
+++ src/main/java/me/manuelp/medialibrarian/Main.java
@@ -1,6 +1,7 @@
 package me.manuelp.medialibrarian;
 
 import fj.data.List;
+import jline.console.ConsoleReader;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
@@ -11,25 +12,31 @@ import me.manuelp.medialibrarian.data.Tag;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 import static fj.data.List.list;
 
 public class Main {
+  private static ConsoleReader console;
+
   public static void main(String[] args) throws IOException,
       InterruptedException {
     Configuration conf = readConfiguration(args);
+    console = createConsole();
 
-    System.out.println("----[ MediaLibrarian welcomes you ]----");
-    System.out.println("Configuration: " + conf.toString());
+    console.println("----[ MediaLibrarian welcomes you ]----");
+    console.println("Configuration: " + conf.toString());
 
     TagsRepository tagsRepository = new SimpleFileTagsRepository(conf
         .getTagsFile().toFile());
     MediaLibrarian librarian = new MediaLibrarian(conf, tagsRepository);
 
     List<Path> files = librarian.findFiles(conf.getDir());
-    System.out.println("Found files: " + files.length());
+    console.println("Found files: " + files.length());
     processFiles(files, librarian);
+  }
+
+  private static ConsoleReader createConsole() throws IOException {
+    return new ConsoleReader();
   }
 
   private static Configuration readConfiguration(String[] args) {
@@ -58,17 +65,20 @@ public class Main {
   private static void processFiles(List<Path> files, MediaLibrarian librarian) {
     files.zipIndex().forEach(
       p -> {
-        System.out.println(String.format("Processing file %d/%d: %s", p._2(),
-          files.length(), p._1().getFileName().toString()));
-        librarian.showFile(p._1());
-        Action a = readAction("Do you wanna archive it? (y/n/d/q)");
-        processAction(p._1(), a, librarian);
+        try {
+          console.println(String.format("Processing file %d/%d: %s", p._2(),
+            files.length(), p._1().getFileName().toString()));
+          librarian.showFile(p._1());
+          Action a = readAction("Do you wanna archive it? (y/n/d/q) ");
+          processAction(p._1(), a, librarian);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
       });
   }
 
-  private static Action readAction(String message) {
-    System.out.println(message);
-    String input = new Scanner(System.in).nextLine();
+  private static Action readAction(String message) throws IOException {
+    String input = console.readLine(message);
     switch (input) {
     case "y":
       return Action.ARCHIVE;
@@ -79,7 +89,7 @@ public class Main {
     case "q":
       return Action.QUIT;
     default:
-      System.out.println("Sorry, I don't understand what you want.");
+      console.println("Sorry, I don't understand what you want.");
       return readAction("Do you wanna archive it? (y/n/d/q)");
     }
   }
@@ -100,8 +110,12 @@ public class Main {
   }
 
   private static List<Tag> readTags(String message) {
-    System.out.println(message);
-    String input = new Scanner(System.in).nextLine();
+    String input;
+    try {
+      input = console.readLine(message);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
     return list(input.split(",")).map(Tag::tag);
   }
 }
